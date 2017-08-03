06d5f3b7bce95e2b57b84ff531203deaef44fb1e
John Mercier <moaxcp@gmail.com>
2017-08-02 22:23:58 -0400

    adding zookeeper and setting up firewall.

--- kafka.nix
+++ kafka.nix
@@ -1,7 +1,14 @@
 {
-  kafkaserver = {
+  kafka = {
     deployment.targetEnv = "container";
     services.apache-kafka.enable = true;
-    networking.firewall.enable = false;
+    networking.firewall.allowedTCPPorts = [ 22 2192 ];
+    services.apache-kafka.zookeeper = "zookeeper:2181";
+  };
+
+  zookeeper = {
+    deployment.targetEnv = "container";
+    services.zookeeper.enable = true;
+    networking.firewall.allowedTCPPorts = [22 2181 ];
   };
 }
